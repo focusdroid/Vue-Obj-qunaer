775a7b89b838ca8eebb037a84129a672e22e8654
focusdroid <840254112@qq.com>
2018-07-14 12:43:48 +0800

    安装sass

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    23132
+    <p>23132</p>
   </div>
 </template>
 
@@ -16,19 +16,10 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style scoped lang="scss">
+.hello{
+  p {
+    font-size:30px;
+  }
 }
 </style>
