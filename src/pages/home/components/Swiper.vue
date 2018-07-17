46f2b3b0eb0e19493c4c26e11e18e580af8e1d49
focusdroid <840254112@qq.com>
2018-07-17 23:45:01 +0800

    轮播图完成

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,14 +2,8 @@
     <div class="swiper">
       <swiper :options="swiperOption">
         <!-- slides -->
-        <swiper-slide>
-          <img class="swiper-img" src="https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg" alt="">
-        </swiper-slide>
-        <swiper-slide>
-          <img class="swiper-img" src="https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/20180713_CPM_11053.jpg" alt="">
-        </swiper-slide>
-        <swiper-slide>
-          <img class="swiper-img" src="https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/anquan_20180716.png" alt="">
+        <swiper-slide v-for="(item) in swiperList" :key="item.id">
+          <img class="swiper-img" :src="item.imgUrl" alt="">
         </swiper-slide>
         <!-- Optional controls -->
         <div class="swiper-pagination"  slot="pagination"></div>
@@ -21,14 +15,34 @@
 export default {
   data () {
     return {
-      swiperOption:{ }
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg'
+      }, {
+        id: '0001',
+        imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/anquan_20180716.png'
+      }]
     }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="stylus">
+  .swiper >>> .swiper-pagination-bullet-active{// >>> 样式穿透
+    background-color: #fff;
+  }
+  .swiper{ // 防止图片加载闪动问题
+    width:100%;
+    height:0;
+    overflow hidden;
+    padding-bottom:31.25%;
+  }
 .swiper-img{
   width: 100%;
 }
+
 </style>
